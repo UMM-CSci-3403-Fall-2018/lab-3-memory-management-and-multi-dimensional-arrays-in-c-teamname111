a9f3277c9ddbae67ac9587f7212482c6215ab10a
EthanUphoff <ethan.uphoff@gmail.com>
2018-10-01 21:10:04 -0500

    Removed current_location_of_null and moved tracking of last variable
    By moving the location of the last element in the found_values array to the beginning there is no longer an issue with comparing 0 to null. array_merge works as of this commit but does not free up memory.

--- array_merge/array_merge.c
+++ array_merge/array_merge.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include <stdbool.h>
 #include <math.h>
 
@@ -11,37 +12,30 @@ int* array_merge(int num_arrays, int* sizes, int** values){
 		j = j + sizes[k];	
 			}
 	int* found_values = (int*) calloc(j+1, sizeof(int));
-	found_values[0]= '\0';
-	int current_location_of_null = 0;
+	found_values[0]= 0;
 	for(int i = 0; i < num_arrays; i++){
 		for(int k = 0; k< sizes[i]; k++){
-				bool l = true;
-				int n = 0;
-				while(found_values[n] != '\0'){
-					if(values[i][k] == found_values[n]){
-					l = false;
-					}
-					n++;
+			bool l = true;
+			for(int n = 1 ; n <= found_values[0]; n++){
+				if(values[i][k] == found_values[n]){
+				l = false;
 				}
-				if(l){
-				found_values[current_location_of_null] = values[i][k];
-				++current_location_of_null;
-				found_values[current_location_of_null] = '\0';
-				}
-				}
+			}
+			if(l){
+				found_values[0] = found_values[0] + 1;	
+				found_values[found_values[0]] = values[i][k];
+			}
+		}
 	}
-
-	int* tempArray = (int*) calloc(current_location_of_null, sizeof(int));
-
-	for(int i = 0; i < current_location_of_null; i++){
+	int* tempArray = (int*) calloc(found_values[0] + 1, sizeof(int));
+	for(int i = 1; i <= found_values[0]; i++){
 		tempArray[i] = found_values[i];
-	}
-	free(found_values);
-	mergesort(current_location_of_null, tempArray);
-	int* result = (int*) calloc(current_location_of_null+1, sizeof(int));
-	result[0] = current_location_of_null;
-	for(int i = 0; i < current_location_of_null+1; i++){
-		result[i+1] = tempArray[i];
-	}
+		}
+	mergesort(found_values[0]+1, tempArray);
+	int* result = (int*) calloc(found_values[0]+1, sizeof(int));
+	result[0] = found_values [0];
+	for(int i = 1; i <= found_values[0]; i++){
+		result[i] = tempArray[i];
+		}       
 	return result;
 }
